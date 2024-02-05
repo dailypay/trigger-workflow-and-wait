50bb6114f58ad9bcf2a2c37c217d1b7a8653995b
marseam <martin.mcgee@dailypay.com>
2024-02-05 17:25:38 +0000

    add debugging statements

--- entrypoint.sh
+++ entrypoint.sh
@@ -123,7 +123,10 @@ get_workflow_runs() {
 
   echo "Getting workflow runs using query: ${query}" >&2
 
-  api "workflows/${INPUT_WORKFLOW_FILE_NAME}/runs?${query}" |
+  response=$(api "workflows/${INPUT_WORKFLOW_FILE_NAME}/runs?${query}")
+  echo "API response: $response" >&2
+
+  echo "$response" |
   jq -r --arg display_title "@${INPUT_DISPLAY_NAME}" '.workflow_runs[] | select(.display_title | contains($display_title)) | .id'
   sort # Sort to ensure repeatable order, and lexicographically for compatibility with join
 }
